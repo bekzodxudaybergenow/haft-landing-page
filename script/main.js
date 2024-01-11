9a60d8bdbae68578b9501f5e3fdb9501eaa38403
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-01-11 06:49:23 +0500

    Add the javascript part of the header section

--- script/main.js
+++ script/main.js
@@ -5,4 +5,5 @@ let headerNav = document.querySelector('.header__nav');
 
 headerMenuBtn.addEventListener('click', () => {
     headerNav.classList.toggle('header__nav--active');
+    headerMenuBtn.classList.toggle('header__menu--close');
 })
