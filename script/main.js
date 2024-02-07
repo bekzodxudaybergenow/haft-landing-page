7efad6b9bd4b806cba2ce2df07288296c768710d
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-02-07 21:28:27 +0500

    Update the javascript part of ourself section

--- script/main.js
+++ script/main.js
@@ -12,4 +12,22 @@ headerMenuBtn.addEventListener('click', () => {
 
 const ourselfSwiper = new Swiper(".ourself__swiper", {
     spaceBetween: 10,
+    breakpoints: {
+        768: {
+            slidesPerView: 1.5,
+            spaceBetween: 20
+        },
+        940: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 2.5,
+            spaceBetween: 20
+        },
+        1440: {
+            slidesPerView: 2.5,
+            spaceBetween: 40
+        }
+    }
 });
